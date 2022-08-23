1fcc983fb7270052eee756893d1cc8858daabadf
refv21 <killahvue@gmail.com>
2022-08-22 22:08:37 -0600

    Agregando MvcPelículas

---
+++ MvcPelicula/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcPelicula.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /HelloWorld/Welcome/
+        public ActionResult Welcome(string nombre, string apellido, int numVeces = 1)
+        {
+            ViewBag.Mensaje = "Hola " + nombre + apellido + ", número de Veces mostrado " + numVeces;
+            ViewBag.NumVeces = 1 + numVeces;
+            return View();
+        }
+    }
+}
